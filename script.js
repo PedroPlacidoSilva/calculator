31db743ebbed6b334d213a2d52588029f7d01643
Pedro Placido Silva <ppspedro@gmail.com>
2022-10-29 22:17:49 +0100

    Updated the inputed value on screen

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 //  Get all number elements and store in variable btns
 let btns = document.getElementsByClassName("number");
 
-firstNumberArr = new Array();
-secondNumberArr = new Array();
+// Array for the value to be used on math operation
+let firstNumberArr = new Array();
+// Last result for the value
+let Total; // Undefined
+
+// Function to get the value of
+function getValue(btnValue) {
+  // If number, adds number to the array firstNumberArr
+  if (
+    btnValue != "/" &&
+    btnValue != "*" &&
+    btnValue != "-" &&
+    btnValue != "+" &&
+    btnValue != "="
+  ) {
+    // Add value to the array
+    firstNumberArr.push(btnValue);
+    // Updates the value to appear on screen
+    const screenValue = document.getElementById("screen"); // Selects the element with div "screen"
+    screenValue.textContent = firstNumberArr.join(""); //Joins the array in a string without commas
+  }
+}
 
 for (const btn of btns) {
   btn.addEventListener("click", () => {
-    // console.log(typeof btn.value);
-    if (
-      btn.value != "/" &&
-      btn.value != "*" &&
-      btn.value != "-" &&
-      btn.value != "+" &&
-      btn.value != "="
-    ) {
-      console.log(btn.value);
-    }
+    getValue(btn.value);
+    console.log(firstNumberArr);
   });
 }
