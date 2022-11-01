cf2178f463ca93f872bf6883e22cabc45ac206f8
Pedro Placido Silva <ppspedro@gmail.com>
2022-11-01 22:45:01 +0000

    Added logic to the calculations. To be done the delete character button funcionality and when user inputs several zeros

--- script.js
+++ script.js
@@ -3,24 +3,93 @@ let btns = document.getElementsByClassName("number");
 
 // Array for the value to be used on math operation
 let firstNumberArr = new Array();
-// Last result for the value
-let Total; // Undefined
 
-// Function to get the value of
+// Array to be used on +-/ operations
+let secondNumberArr = new Array();
+
+// Last total value of the operation
+let total = 0; // Undefined
+
+// Stores the math operation to be made
+let operation = undefined;
+
+// Selects the element with div "screen"
+const screenValue = document.getElementById("screen");
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 function getValue(btnValue) {
-  // If number, adds number to the array firstNumberArr
-  if (
-    btnValue != "/" &&
-    btnValue != "*" &&
-    btnValue != "-" &&
-    btnValue != "+" &&
-    btnValue != "="
-  ) {
-    // Add value to the array
-    firstNumberArr.push(btnValue);
-    // Updates the value to appear on screen
-    const screenValue = document.getElementById("screen"); // Selects the element with div "screen"
-    screenValue.textContent = firstNumberArr.join(""); //Joins the array in a string without commas
+  // Add elements to firstNumberArray until we had a "+-/*" user input
+  if (btnValue == "AC") {
+    // IF User press AC
+    // Update the screen to be "0"
+    screenValue.textContent = "0";
+    // Update the variable operation to be empty
+    operation = undefined;
+    // Update firstNumberArray and secondNumberArray to empty Array
+    firstNumberArr = [];
+    secondNumberArr = [];
+    // Update total variable to zero
+    total = "";
+  } else if (btnValue == "-" && firstNumberArr.length == 0) {
+    //If user press minus sign and there is no number inputed update first number to "0"
+    firstNumberArr.push(0);
+    operation = btnValue;
+  } else if (!isNaN(+btnValue) || btnValue == ".") {
+    // If user press a number or "."
+    // Verifies if any operation as been inputed by the user. Update first Number if No; else update second Number
+    console.log(operation);
+    if (operation == undefined || operation == "=") {
+      if (operation == "=") {
+        firstNumberArr = [];
+        operation = undefined;
+      }
+      firstNumberArr.push(btnValue);
+      screenValue.textContent = firstNumberArr.join(""); //Joins the array in a string without commas
+    } else {
+      secondNumberArr.push(btnValue);
+      screenValue.textContent = secondNumberArr.join(""); //Joins the array in a string without commas
+    }
+  } else if (["+", "-", "*", "/", "="].indexOf(btnValue) >= 0) {
+    //If the values of array not in btnValue the result will be -1.
+    if (secondNumberArr.length > 0) {
+      // If second number already inputed
+      switch (operation) {
+        case "+":
+          total = add(+firstNumberArr.join(""), +secondNumberArr.join(""));
+          break;
+        case "-":
+          total = subtract(+firstNumberArr.join(""), +secondNumberArr.join(""));
+          break;
+        case "*":
+          total = multiply(+firstNumberArr.join(""), +secondNumberArr.join(""));
+          break;
+        case "/":
+          total = divide(+firstNumberArr.join(""), +secondNumberArr.join(""));
+          break;
+      }
+      // Update screen with the result
+      screenValue.textContent = total;
+      // Update first number to be equal to the result and update the second number to nothing
+      firstNumberArr = [];
+      firstNumberArr.push(total);
+      secondNumberArr = [];
+    }
+    operation = btnValue;
   }
 }
 
